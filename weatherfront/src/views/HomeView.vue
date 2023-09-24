24c38e0455f4e3cc1fc85e41442cfffda191ae89
TAIRI TIIMANN <tairiti@gmail.com>
2023-09-24 19:54:06 +0300

    wip

--- weatherfront/src/views/HomeView.vue
+++ weatherfront/src/views/HomeView.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ilm</h1>
+
+
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Sisesta linna nimi:
+      </div>
+
+      <div class="col">
+        <input v-model="cityName">
+      </div>
+      <br>
+      <div class="col">
+        <button @click="getWeatherInfo">Küsi ilma andmeid</button>
+      </div>
+      <br>
+      <div class="col">
+       Temperatuur: {{weatherResponse.temperature}} °C
+      </div>
+      <div class="col">
+       Tuulekiirus: {{weatherResponse.windSpeed}} m/s
+      </div>
+      <div class="col">
+       Õhuniiskus {{weatherResponse.humidity}} %
+      </div>
+      <br>
+
+      <div class="col">
+       Kustuta linn ja andmed {{cityName }}
+        <br>
+
+        <button @click="deleteCityAndWeatherInfo">Kustuta</button>
+      </div>
+
+
+    </div>
   </div>
+
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      cityName: '',
+      weatherResponse: {
+        temperature: 0,
+        windSpeed: 0,
+        humidity: 0
+      }
+    }
+  },
+  methods: {
+
+    getWeatherInfo() {
+      this.$http.get("/weather", {
+            params: {
+              cityName: this.cityName
+            }
+          }
+      ).then(response => {
+        this.weatherResponse = response.data
+      }).catch(error => {
+        const errorResponseBody = error.response.data
+      })
+    },
+    deleteCityAndWeatherInfo() {
+      this.$http.delete("/weather", {
+            params: {
+              cityName: this.cityName
+            }
+          }
+      ).then(response => {
+       alert("Linn ja ilmaandmed andmebaasist kustutatud")
+        this.resetAllFields();
+      }).catch(error => {
+        const errorResponseBody = error.response.data
+      })
+    },
+    resetAllFields() {
+      this.cityName = ''
+      this.weatherResponse.temperature = 0
+      this.weatherResponse.windSpeed = 0
+      this.weatherResponse.humidity = 0
+    },
   }
 }
 </script>
